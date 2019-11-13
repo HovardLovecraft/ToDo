53cdd36183cd6b2fcd36930118c004e05a61f590
Ivan Kuznetsov1 <ivan_kuznetsov1@epam.com>
2019-11-14 01:12:50 +0200

    init commit

--- myScript.js
+++ myScript.js
@@ -1,13 +1,9 @@
-let link = document.querySelector('a'); // в середине тега <a> по css селектору ищет...  будет соответствовать первому элементу <a>
-// Document.querySelectorAll('a') будет соответствовать всем элементам <a>
-link.textContent = 'Mozilla Developer Network';
-link.href = 'https://developer.mozilla.org';
-
-let sect = document.querySelector('section');
-let para = document.createElement('p');
-para.textContent = 'We hope you enjoyed the ride.';
-sect.appendChild(para);
-
-let text = document.createTextNode(' — the premier source for web development knowledge.');
-let linkPara = document.querySelector('p');
-linkPara.appendChild(text);
+form.onclick = function(event) {
+    event.target.style.backgroundColor = 'yellow';
+  
+    // браузеру нужно некоторое время, чтобы зарисовать всё жёлтым
+    setTimeout(() => {
+      alert("target = " + event.target.tagName + ", this=" + this.tagName);
+      event.target.style.backgroundColor = ''
+    }, 0);
+  };
